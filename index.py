ba86685ef892ea012bba34640cfc449bebc32fbc
yashrajput7859-oss <yashrajput7859@gmail.com>
2025-12-30 22:52:12 +0530

    added addition code

--- index.py
+++ index.py
@@ -1,14 +1,4 @@
-a= 102
-b= 20
-sum= a + b
-diff= a-b
-mult= a*b
-div= b/a
-Mod= b%a
-
-print("The sum of", a, "and", b, "is", sum)
-print("The difference of", a, "and", b, "is", diff)
-print("The product of", a, "and", b, "is", mult)    
-print("The division of", b, "by", a, "is", div)
-print("The modulus of", b, "by", a, "is", Mod)
-# This script calculates the sum of two numbers and prints the result.
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+sum_result = a + b
+print("The sum of", a, "and", b, "is:", sum_result)
