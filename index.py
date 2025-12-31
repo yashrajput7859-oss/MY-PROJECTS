e87c6baf820fd268b7510630df34c098226d6702
yashrajput7859-oss <yashrajput7859@gmail.com>
2025-12-31 12:12:46 +0530

    added rows

--- index.py
+++ index.py
@@ -2,3 +2,6 @@ a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
 sum_result = a + b
 print("The sum of", a, "and", b, "is:", sum_result)
+print("This is a simple addition program.")
+print("The remainder is:", a % b)   
+print("The remainder when", n1, "is divided by", n2, "is:", n1 % n2)    
